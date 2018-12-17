7d531cce36e4ac4f66af95af4f4c6fc876810033
Mehul Kanani <mehulkanani09@gmail.com>
2018-12-17 21:11:52 +0530

    Create studies1.js

---
+++ studies1.js
@@ -0,0 +1,18 @@
+var obj = {
+	firstname : "Mehul",
+	lastname : "Kanani",
+	rank : 1,
+	marks : {
+		maths : 70,
+		scince : 80,
+		english : 85,
+	}
+}
+
+var fun1 = function (var) 
+{
+	firstname : MEHUL,
+	lastname : KANANI,
+}
+fun1(obj);
+console.log(obj)
